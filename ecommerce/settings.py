3d4f35d7f3f751910e61e8be42e7d3c37b0aeb61
BruceNguyen01 <bruce.nguyen.goldenowl@gmail.com>
2022-11-25 11:33:39 +0700

    whitenoise

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,6 +84,7 @@ SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware', # new, for language
     'django.middleware.common.CommonMiddleware',
@@ -344,3 +345,4 @@ DEBUG_TOOLBAR_CONFIG = {
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
