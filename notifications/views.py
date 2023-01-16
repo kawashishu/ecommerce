432df371a1159f05548bdf7438ace8a41b76b96c
BruceNguyen01 <bruce.nguyen.goldenowl@gmail.com>
2023-01-16 17:36:25 +0700

    test realtime

---
+++ notifications/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+# chat/views.py
+from django.shortcuts import render
+
+# chat/views.py
+from django.shortcuts import render
+
+
+def chat(request):
+    return render(request, "chat.html")
+
+
+def room(request, room_name):
+    return render(request, "room.html", {"room_name": room_name})
